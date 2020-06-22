573960aeab634ff9881c82bed2805bfd686cb404
alain houenou <alain.softservices@gmail.com>
2020-06-22 08:09:50 +0200

    premier modification

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+//import firebase  from 'firebase';
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAcqGuKGDzzq52xLl8Eis9NOdSnzw4M6Kw",
+    authDomain: "vueshop-eb16a.firebaseapp.com",
+    databaseURL: "https://vueshop-eb16a.firebaseio.com",
+    projectId: "vueshop-eb16a",
+    storageBucket: "vueshop-eb16a.appspot.com",
+    messagingSenderId: "420377496480",
+    appId: "1:420377496480:web:5174cb765bd993576872c5",
+    measurementId: "G-C1SZXHX8PW"
+  };
+  // Initialize Firebase
+  fff;
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
